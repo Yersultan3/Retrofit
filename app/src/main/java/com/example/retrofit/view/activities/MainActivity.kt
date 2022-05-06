3e9704917d96b34f58d1ef48ecc71f2355010870
yersultan3.yk <yersultan3.yk@gmail.com>
2022-05-06 18:42:46 +0600

    some changes

--- app/src/main/java/com/example/retrofit/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/view/activities/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.retrofit.view.activities
 
-import android.content.Context
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.app.PendingIntent.getActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toolbar
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -21,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+//    private var fragment = LoginFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,9 +41,15 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigation.setupWithNavController(navController)
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-    }
+//    override fun onBackPressed() {
+//
+//        val manager: FragmentManager = this.supportFragmentManager
+//        val trans = manager.beginTransaction()
+//        trans.remove(Fragment())
+//        trans.commit()
+//        manager.popBackStack()
+//        super.onBackPressed()
+//    }
 
 
 }
--- app/src/main/java/com/example/retrofit/view/fragments/DetailFragment.kt
+++ app/src/main/java/com/example/retrofit/view/fragments/DetailFragment.kt
@@ -88,6 +88,7 @@ class DetailFragment: Fragment() {
             imageActive()
             binding.tvTitle.text = it.title
             binding.tvOverview.text = it.overview
+            binding.releaseDate.text = it.release_date
 //            binding.swipeRefresh.isRefreshing = false
         }
     }
